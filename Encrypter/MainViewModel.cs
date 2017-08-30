f8a7a1b6d81bce9b33a755c736ca8f45033aab18
Ricardo Santos <ricardo.santos@bitflyer.com>
2017-08-30 15:31:56 +0900

    initial commit, should be fully functional

---
+++ Encrypter/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Encrypter
+{
+    internal class SampleData : MainViewModel
+    {
+        public SampleData()
+        {
+            TextToEncrypt = "abc";
+        }
+    }
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    internal class MainViewModel : ViewModelBase
+    {
+        private string encryptedText;
+        private string textToDecrypt;
+
+        public MainViewModel()
+        {
+
+        }
+
+        public string TextToEncrypt
+        {
+            get { return textToDecrypt; }
+            set
+            {
+                try
+                {
+                    textToDecrypt = value;
+                    OnPropertyChanged();
+
+                    if (textToDecrypt.StartsWith(":"))
+                    {
+                        var textToDecrypt1 = textToDecrypt.Split(':')[1];
+                        if (!string.IsNullOrEmpty(textToDecrypt1))
+                        { EncryptedText = Encryption.AESThenHMAC.SimpleDecryptWithPassword(textToDecrypt1, "passwordMustBe12"); }
+                        else
+                        {
+                            EncryptedText = string.Empty;
+                        }
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(textToDecrypt))
+                        { EncryptedText = Encryption.AESThenHMAC.SimpleEncryptWithPassword(textToDecrypt, "passwordMustBe12"); }
+                        else
+                        {
+                            EncryptedText = string.Empty;
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    EncryptedText = string.Empty;
+                    Debug.WriteLine(exc.Message);
+                }
+            }
+        }
+        public string EncryptedText { get { return encryptedText; } set { encryptedText = value; OnPropertyChanged(); } }
+    }
+}
+
---
+++ Encrypter/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Encrypter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
